cf276240019ac0e5f4b25c4aa67fbe4989ee4871
Gary Zhao <1815247@qq.com>
2020-10-20 18:52:51 +0800

    update variables.tf

--- variables.tf
+++ variables.tf
@@ -2,6 +2,60 @@ variable "region" {
   default = "us-east-1"
 }
 
+variable "vpc_name" {
+  description = "VPC name"
+  default = "kubeflow-terraform-vpc"
+}
+
+variable "vpc_cidr" {
+  description = "VPC IP scope"
+  default = "172.16.0.0/16"
+}
+
+variable "private_subnets" {
+  description = "Private subnets IP scope"
+  type        = list(string)
+
+  default = ["172.16.1.0/24", "172.16.2.0/24", "172.16.3.0/24"]
+}
+
+variable "public_subnets" {
+  description = "Public subnets IP scope"
+  type        = list(string)
+
+  default = ["172.16.4.0/24", "172.16.5.0/24", "172.16.6.0/24"]
+}
+
+variable "cluster_name" {
+  description = "EKS Cluster name"
+  default = "kubeflow-eks-terraform"
+}
+
+variable "cluster_env" {
+  description = "Cluster env label name"
+  default = "test"
+}
+
+variable "node_desired_capacity" {
+  description = "EKS node desired capacity"
+  default = "4"
+}
+
+variable "node_max_capacity" {
+  description = "EKS node max capacity"
+  default = "10"
+}
+
+variable "node_min_capacity" {
+  description = "EKS node min capacity"
+  default = "2"
+}
+
+variable "node_ec2_type" {
+  description = "EKS node ec2 instance type"
+  default = "m5.large"
+}
+
 variable "map_accounts" {
   description = "Additional AWS account numbers to add to the aws-auth configmap."
   type        = list(string)
